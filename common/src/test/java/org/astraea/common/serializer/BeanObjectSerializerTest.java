b5d6c3e3a00a78aab0ef0e6325330bee224f3ddf
chinghongfang <fjh7777@gmail.com>
2023-04-05 15:33:35 +0800

    Add deserialization

--- common/src/test/java/org/astraea/common/serializer/BeanObjectSerializerTest.java
+++ common/src/test/java/org/astraea/common/serializer/BeanObjectSerializerTest.java
@@ -18,40 +18,49 @@ package org.astraea.common.serializer;
 
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
+import java.util.HashMap;
 import java.util.Map;
 import org.apache.avro.io.BinaryEncoder;
 import org.apache.avro.io.DatumWriter;
+import org.apache.avro.io.Decoder;
+import org.apache.avro.io.DecoderFactory;
 import org.apache.avro.io.EncoderFactory;
+import org.apache.avro.specific.SpecificDatumReader;
 import org.apache.avro.specific.SpecificDatumWriter;
 import org.astraea.common.Utils;
 import org.astraea.common.producer.Bean;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class BeanObjectSerializerTest {
   @Test
   void testing() throws IOException {
     String domain = "topicA";
-    Map<CharSequence, CharSequence> properties = Map.of("name", "publisher");
-    Map<CharSequence, Object> attributes = Map.of("value", "3");
+    Map<CharSequence, CharSequence> properties = new HashMap<>(Map.of("name", "publisher"));
+    Map<CharSequence, Object> attributes = new HashMap<>(Map.of("value", "3"));
 
     ByteArrayOutputStream os = new ByteArrayOutputStream();
     BinaryEncoder encoder = EncoderFactory.get().binaryEncoder(os, null);
     DatumWriter<Bean> beanWriter = new SpecificDatumWriter<>(Bean.class);
     // List<byte[]> trash = new ArrayList<>(1000);
+    var reader = new SpecificDatumReader<>(Bean.class);
 
     long start = System.currentTimeMillis();
-    //for (int i = 0; i < 100_000_000; ++i) {
+    for (int i = 0; i < 100_000_000; ++i) {
       os.reset();
-      Bean.Builder builder = Bean.newBuilder().setDomain(Utils.randomString(5));
+      Bean.Builder builder = Bean.newBuilder().setDomain(domain);
       builder.setProperties(properties);
       builder.setAttributes(attributes);
       Bean bean = builder.build();
 
       beanWriter.write(bean, encoder);
       encoder.flush();
-    //}
-    var bytes = os.toByteArray();
+      var bytes = os.toByteArray();
+
+      Decoder decoder = DecoderFactory.get().binaryDecoder(bytes, null);
+      Bean deserialized = reader.read(null, decoder);
+      Assertions.assertEquals(domain, deserialized.getDomain().toString());
+    }
     System.out.println("  serialize time: " + (System.currentTimeMillis() - start) + " ms");
-    System.out.println("  serialized size: "+ bytes.length);
   }
 }
